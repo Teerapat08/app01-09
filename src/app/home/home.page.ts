063c3174cbf8aa7b640831d67daff5aa73c4a291
Teerapat koaprajun <Teerapat.liv@gmail.com>
2024-07-24 14:29:42 +0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonGrid, IonRow, IonCol, IonButton } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonItem, IonLabel, IonGrid, IonRow, IonCol, IonButton],
 })
 export class HomePage {
   constructor() {}
